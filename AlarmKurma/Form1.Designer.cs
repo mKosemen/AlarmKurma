e12a1653d2c132d249444179e10933bcf7ca27a0
mKosemen <103363649+mKosemen@users.noreply.github.com>
2022-06-05 19:37:16 +0300

    Add project files.

---
+++ AlarmKurma/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace AlarmKurma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AlarmTimer = new System.Windows.Forms.Timer(this.components);
+            this.LocalTimer = new System.Windows.Forms.Timer(this.components);
+            this.ArkaPlanTimer = new System.Windows.Forms.Timer(this.components);
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.lblAlarm = new System.Windows.Forms.Label();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.btnKur = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.pnlKontrol = new System.Windows.Forms.Panel();
+            this.pnlKontrol.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AlarmTimer
+            // 
+            this.AlarmTimer.Interval = 1000;
+            this.AlarmTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // LocalTimer
+            // 
+            this.LocalTimer.Tick += new System.EventHandler(this.LocalTimer_Tick);
+            // 
+            // ArkaPlanTimer
+            // 
+            this.ArkaPlanTimer.Interval = 300;
+            this.ArkaPlanTimer.Tick += new System.EventHandler(this.ArkaPlanTimer_Tick);
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.CalendarTitleForeColor = System.Drawing.Color.YellowGreen;
+            this.dtpTarih.CustomFormat = "";
+            this.dtpTarih.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTarih.Location = new System.Drawing.Point(129, 16);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.ShowUpDown = true;
+            this.dtpTarih.Size = new System.Drawing.Size(165, 20);
+            this.dtpTarih.TabIndex = 5;
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Font = new System.Drawing.Font("MS Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaat.Location = new System.Drawing.Point(278, 33);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(0, 21);
+            this.lblSaat.TabIndex = 6;
+            // 
+            // lblAlarm
+            // 
+            this.lblAlarm.AutoSize = true;
+            this.lblAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAlarm.Location = new System.Drawing.Point(23, 16);
+            this.lblAlarm.Name = "lblAlarm";
+            this.lblAlarm.Size = new System.Drawing.Size(100, 16);
+            this.lblAlarm.TabIndex = 1;
+            this.lblAlarm.Text = "Alarm Zamanı : ";
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Location = new System.Drawing.Point(233, 73);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(0, 13);
+            this.lblMesaj.TabIndex = 4;
+            // 
+            // btnKur
+            // 
+            this.btnKur.Location = new System.Drawing.Point(332, 13);
+            this.btnKur.Name = "btnKur";
+            this.btnKur.Size = new System.Drawing.Size(75, 23);
+            this.btnKur.TabIndex = 2;
+            this.btnKur.Text = "Alarm Kur";
+            this.btnKur.UseVisualStyleBackColor = true;
+            this.btnKur.Click += new System.EventHandler(this.btnKur_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Enabled = false;
+            this.btnDurdur.Location = new System.Drawing.Point(434, 13);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(107, 23);
+            this.btnDurdur.TabIndex = 3;
+            this.btnDurdur.Text = "Alarmı Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            // 
+            // pnlKontrol
+            // 
+            this.pnlKontrol.Controls.Add(this.dtpTarih);
+            this.pnlKontrol.Controls.Add(this.btnKur);
+            this.pnlKontrol.Controls.Add(this.lblAlarm);
+            this.pnlKontrol.Controls.Add(this.lblMesaj);
+            this.pnlKontrol.Controls.Add(this.btnDurdur);
+            this.pnlKontrol.Location = new System.Drawing.Point(26, 57);
+            this.pnlKontrol.Name = "pnlKontrol";
+            this.pnlKontrol.Size = new System.Drawing.Size(556, 120);
+            this.pnlKontrol.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 397);
+            this.Controls.Add(this.pnlKontrol);
+            this.Controls.Add(this.lblSaat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlKontrol.ResumeLayout(false);
+            this.pnlKontrol.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer AlarmTimer;
+        private System.Windows.Forms.Timer LocalTimer;
+        private System.Windows.Forms.Timer ArkaPlanTimer;
+        private System.Windows.Forms.DateTimePicker dtpTarih;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label lblAlarm;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.Button btnKur;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Panel pnlKontrol;
+    }
+}
+
---
+++ AlarmKurma/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+namespace AlarmKurma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        SoundPlayer player = new SoundPlayer();
+        PictureBox pbDurdur = new PictureBox();
+        private void btnKur_Click(object sender, EventArgs e)
+        {
+            AlarmTimer.Start();
+            lblMesaj.Text = "Alarm Kuruldu.";
+            btnDurdur.Enabled = false;
+
+            pbDurdur.SetBounds(228, 179, 90, 90);
+            this.Controls.Add(pbDurdur);
+            pbDurdur.Image = Properties.Resources.DilCikaranCocuk;
+            pbDurdur.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbDurdur.Visible = false;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            DateTime alarmTime = dtpTarih.Value;
+            if (dateTime.Hour == alarmTime.Hour && dateTime.Minute == alarmTime.Minute && dateTime.Second == alarmTime.Second)
+            {
+                AlarmTimer.Stop();
+                lblMesaj.Text = "Alarm Çalıyor...";
+                btnDurdur.Enabled = true;
+                Stream alarm = Properties.Resources.TavukAlarm;
+                SoundPlayer cal = new SoundPlayer(alarm);
+                cal.PlayLooping();
+                ArkaPlanTimer.Start();
+                pnlKontrol.Visible = false;
+                pbDurdur.Visible = true;
+            }
+        }
+        private void LocalTimer_Tick(object sender, EventArgs e)
+        {
+            lblSaat.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+        Random r = new Random();
+        private void ArkaPlanTimer_Tick(object sender, EventArgs e)
+        {
+            int x = rnd.Next(255);
+            int y = rnd.Next(255);
+            int z = rnd.Next(255);
+            this.BackColor = Color.FromArgb(x, y, z);
+            pbDurdur.Top = r.Next(0, this.Height - 50);
+            pbDurdur.Left = r.Next(0, this.Width - 50);
+        }
+        private void PbDurdur_Click(Object sender, EventArgs e)
+        {
+            player.Stop();
+            lblMesaj.Text = "Alarm Durduruldu.";
+            btnKur.Enabled = true;
+            ArkaPlanTimer.Stop();
+            this.BackColor = Color.White;
+            pnlKontrol.Visible = true;
+            pbDurdur.Image = Properties.Resources.cocuk_hareketli_resim_0108;
+            pbDurdur.SetBounds(228, 179, 120, 200);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LocalTimer.Start();
+            pbDurdur.Click += new EventHandler(PbDurdur_Click);
+        }
+
+    }
+}
